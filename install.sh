1bbe15424cc5cdbd7fa7f7f8e3392a40df005f83
FirkAlex22 <46854202+FirkAlex22@users.noreply.github.com>
2019-05-13 06:40:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ echo "Updating"
 apt-get update > install.log
 apt-get -y install python &>> install.log
 apt-get -y install python3 &>> install.log
+pip install os &>> install.log
 pip install colorama &>> install.log
 pip install random &>> install.log
 chmod 777 *
