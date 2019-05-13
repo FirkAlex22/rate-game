7db33f2569db971f697aede797d92474faccb52d
FirkAlex22 <46854202+FirkAlex22@users.noreply.github.com>
2019-05-13 06:39:55 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Updating"
+apt-get update > install.log
+apt-get -y install python &>> install.log
+apt-get -y install python3 &>> install.log
+pip install colorama &>> install.log
+pip install random &>> install.log
+chmod 777 *
+echo "nstalled"
