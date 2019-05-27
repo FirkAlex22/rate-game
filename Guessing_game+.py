af14718b4e6db7fb55ff192623aebe05d3a70275
FirkAlex22 <46854202+FirkAlex22@users.noreply.github.com>
2019-05-27 20:09:40 +0300

    Update Guessing_game+.py

--- Guessing_game+.py
+++ Guessing_game+.py
@@ -2,7 +2,8 @@ from Crypt_num_for_Guessing_game import crypt, decrypt
 import random, os
 from colorama import init, Back, Fore, Style
 init()
-opt_file = open("opt_file.txt", mode = "r+", encoding="latin_1")
+text_file = "a)/opt_file.txt"
+opt_file = open(text_file, mode = "r+", encoding="latin_1")
 points_s_opt_file = opt_file.read()
 points = points_s_opt_file
 points = decrypt(points)
@@ -14,7 +15,7 @@ if True:
 def cheat_money():
     global points, opt_file, money, decrypt, crypt
     print(Fore.RED + "[cheat]" + Style.RESET_ALL + Fore.YELLOW + "how many it is necessary for you money?" + Style.RESET_ALL)
-    opt_file = open("opt_file.txt", mode = "r+", encoding="latin_1")
+    opt_file = open(text_file, mode = "r+", encoding="latin_1")
     money = int(input("> "))
     points = int(points) + money
     opt_file.seek(0)
@@ -63,7 +64,7 @@ while skip_w:
                     print(Fore.GREEN + "[+]" + Style.RESET_ALL + Fore.RED + "You have won a double bet!!!! The number was " + str(random_num) + Style.RESET_ALL)
                 elif client_num != random_num:
                     points = int(points) - int(rate)
-                    opt_file = open("opt_file.txt", mode = "r+", encoding="latin_1")
+                    opt_file = open(text_file, mode = "r+", encoding="latin_1")
                     print(Fore.GREEN + "[-]" + Style.RESET_ALL + Fore.RED + "You lost the bet, your balance: " + str(points) + Style.RESET_ALL)
                     print(Fore.GREEN + "[!]" + Style.RESET_ALL + Fore.CYAN + "number was " + str(random_num) + Style.RESET_ALL)
                     opt_file.seek(0)
@@ -77,7 +78,7 @@ while skip_w:
                     elif skip == "//cheat_money":
                         cheat_money()
                     elif skip != "" or (skip == "exit" or skip == "/exit"):
-                        opt_file = open("opt_file.txt", mode = "r+", encoding="latin_1")
+                        opt_file = open(text_file, mode = "r+", encoding="latin_1")
                         opt_file.seek(0)
                         points = crypt(points)
                         opt_file.write(str(points))
